e3496e8ac0c09d04270ef90a0a19ba4e984deffd
Simon <simon@benezan.de>
2022-06-18 11:43:01 +0200

    basic stuff

--- src/api.ts
+++ src/api.ts
@@ -1,35 +1,54 @@
-import { type v1 } from "moos-api";
-import fetch, { Response, RequestInit } from "node-fetch";
 import cookie from "cookie";
+import { type v1 } from "moos-api";
+import fetch, { RequestInit, Response } from "node-fetch";
 
 let csrfToken: string | null = null;
 let csrfCookie: string | null = null;
+
+export async function createCollection(
+  options: v1.operations["put-profile-collection"]["requestBody"]["content"]["application/json"]
+): Promise<v1.SeasonGroup | null> {
+  return ((await _csrfFetch("/profile/collection", options, { method: "PUT" }).then((response) => response.json())) as v1.SeasonGroup) ?? null;
+}
+
+export async function createSeason(
+  options: v1.operations["put-profile-list"]["requestBody"]["content"]["application/json"]
+): Promise<v1.Season | null> {
+  return ((await _csrfFetch("/profile/list", options, { method: "PUT" }).then((response) => response.json())) as v1.Season) ?? null;
+}
+
+export async function createEpisode(
+  options: v1.operations["put-profile-episode"]["requestBody"]["content"]["application/json"]
+): Promise<v1.Episode | null> {
+  return ((await _csrfFetch("/profile/episode", options, { method: "PUT" }).then((response) => response.json())) as v1.Episode) ?? null;
+}
+
+export async function createSource(
+  options: v1.operations["put-profile-source"]["requestBody"]["content"]["application/json"]
+): Promise<v1.Source | null> {
+  return ((await _csrfFetch("/profile/source", options, { method: "PUT" }).then((response) => response.json())) as v1.Source) ?? null;
+}
+
+export async function createFile(
+  options: v1.operations["put-profile-file"]["requestBody"]["content"]["application/json"]
+): Promise<v1.operations["put-profile-file"]["responses"]["200"]["content"]["application/json"] | null> {
+  return (
+    ((await _csrfFetch("/profile/file", options, { method: "PUT" }).then((response) =>
+      response.json()
+    )) as v1.operations["put-profile-file"]["responses"]["200"]["content"]["application/json"]) ?? null
+  );
+}
+
+export async function deleteFile(options: v1.operations["delete-profile-file"]["requestBody"]["content"]["application/json"]): Promise<boolean> {
+  return (await _csrfFetch("/profile/file", options, { method: "DELETE" }).then((response) => response.ok)) ?? false;
+}
 
 export async function fetchProfile(): Promise<v1.UserProfile | null> {
   return ((await _csrfFetch("/profile", undefined, { method: "POST" }).then((response) => response.json())) as v1.UserProfile) ?? null;
 }
 
-export async function patchProfile(parameters: v1.operations["patch-profile"]["requestBody"]["content"]["application/json"]): Promise<boolean> {
-  return _csrfFetch("/profile", parameters, { method: "PATCH" }).then((response) => response.ok);
-}
-
-export async function requestSessionCookie(idToken: string): Promise<boolean> {
-  const body: v1.operations["request-session"]["requestBody"]["content"]["application/json"] = {
-    token: idToken
-  };
-  return _csrfFetch("/session", body, { method: "POST" }).then((response) => response.ok);
-}
-
-export async function revokeSessionCookie(): Promise<boolean> {
-  return _csrfFetch("/session", undefined, { method: "DELETE" }).then((response) => response.ok);
-}
-
 export async function requestCSRFToken(): Promise<Response> {
   return _fetch("/csrf-token", undefined, { method: "GET" });
-}
-
-export async function testCSRFToken(): Promise<Response> {
-  return _csrfFetch("/csrf-token", undefined, { method: "POST" });
 }
 
 async function _csrfFetch(endpoint: keyof v1.paths, body?: object, init?: RequestInit, retry = true): Promise<Response> {
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
-import type * as CDNTypes from "./cdn-types.js";
 import dotenv from "dotenv";
 import { existsSync, readFileSync } from "fs";
+import { v1 } from "moos-api";
+import fetch, { fileFromSync } from "node-fetch";
 import readline from "readline";
-import { fetchProfile } from "./api.js";
+import { createCollection, createEpisode, createFile, createSeason, createSource, deleteFile, fetchProfile } from "./api.js";
+import type * as CDNTypes from "./cdn-types.js";
 
 dotenv.config();
 
@@ -19,7 +21,7 @@ const question = async (question: string): Promise<string> => {
 
 async function start() {
   const path = await question("Path: ");
-  if (!existsSync(path)) {
+  if (!existsSync(path) || !existsSync(`${path}/media`)) {
     console.error("Path does not exist");
     return;
   }
@@ -31,17 +33,88 @@ async function start() {
     for (const playlistPreview of playlists.playlists) {
       if (!acceptAll) {
         const handlePlaylist = await question(`Process ${playlistPreview.name}? (y/n/a) `);
-        if (handlePlaylist !== "y" && handlePlaylist !== "a") {
+        if (handlePlaylist !== "y" && handlePlaylist !== "Y" && handlePlaylist !== "a" && handlePlaylist !== "A") {
           return;
-        } else if (handlePlaylist === "a") {
+        } else if (handlePlaylist === "a" || handlePlaylist === "A") {
           acceptAll = true;
         }
       }
       try {
         const playlist: CDNTypes.PlaylistJson = JSON.parse(readFileSync(`${path}/playlists/${playlists.dictionary[playlistPreview.key]}`, "utf8"));
-        console.log(fetch);
-
-        console.log(playlist);
+        const collection = await createCollection({
+          name: playlist.name,
+          visibility: "unlisted",
+          thumbnail: thumbnails[playlist.key] ? await uploadFile(`${path}/thumbnails/${thumbnails[playlist.key]}`, `${playlist.name}.jpg`) : undefined
+        });
+        if (!collection) {
+          throw new Error("Collection creation failed");
+        }
+        console.log(collection);
+        const seasons: v1.Season[] = [];
+        for (const season of playlist.seasons) {
+          if (!seasons[season.index]) {
+            const newSeason = await createSeason({
+              groupId: collection?.id,
+              index: season.index
+            });
+            if (!newSeason) {
+              throw new Error("Season creation failed");
+            }
+            seasons[season.index] = newSeason;
+          }
+        }
+        const episodes: { [key: string]: v1.Episode } = {};
+        for (const season of playlist.seasons) {
+          for (const episode of season.episodes) {
+            if (!seasons[season.index].episodes.map((episodeId) => episodes[episodeId]).find((e) => e.index === episode.index)) {
+              const newEpisode = await createEpisode({
+                index: episode.index,
+                name: episode.name,
+                seasonId: seasons[season.index].id
+              });
+              if (!newEpisode) {
+                console.error(`Episode creation failed, ${season.index},${episode.index}:${episode.name}`);
+                continue;
+              }
+              episodes[newEpisode.id] = newEpisode;
+              seasons[season.index].episodes.push(newEpisode.id);
+            }
+            const fetchedEpisode = seasons[season.index].episodes.map((episodeId) => episodes[episodeId]).find((e) => e.index === episode.index);
+            if (!fetchedEpisode) {
+              console.error(`Episode disappeared, ${season.index},${episode.index}:${episode.name}`);
+              continue;
+            }
+            try {
+              const sourceFile = await uploadFile(
+                `${path}/media/${playlist.dictionary[episode.key]}`,
+                `${playlist.name} S${season.index} ${episode.name}.mp4`
+              );
+              const source = await createSource({
+                episodeId: fetchedEpisode.id,
+                language: season.language.includes("-") ? languageIndexToEnum(season.language.split("-")[0]) : languageIndexToEnum(season.language),
+                seasonId: seasons[season.index].id,
+                subtitles: season.language.includes("-") ? languageIndexToEnum(season.language.split("-")[1]) : undefined,
+                name: fetchedEpisode.name !== episode.name ? episode.name : undefined,
+                key: sourceFile
+              });
+              if (!source) {
+                try {
+                  await deleteFile({ id: sourceFile });
+                } catch (error) {
+                  console.error(error);
+                }
+                console.error(`Source creation failed, ${season.index},${episode.index}:${episode.name}`);
+                continue;
+              }
+              fetchedEpisode.sources.push(source);
+            } catch (error) {
+              console.error(error);
+              console.error(`Source Upload failed, ${season.index},${episode.index}:${episode.name}`);
+            }
+          }
+        }
+        console.log(seasons);
+        console.log(episodes);
       } catch (error) {
         console.error(error);
         console.error(`Failed to process ${playlistPreview.name}`);
@@ -50,6 +123,43 @@ async function start() {
   } catch (error) {
     console.error(error);
   }
+}
+
+function languageIndexToEnum(language: string): v1.Language {
+  return `${language}` === "0"
+    ? "de_DE"
+    : `${language}` === "1"
+    ? "ja_JP"
+    : `${language}` === "2"
+    ? "en_EN"
+    : `${language}` === "3"
+    ? "zh_CN"
+    : "en_EN";
+}
+
+async function uploadFile(filePath: string, fileName: string): Promise<string> {
+  const target = await createFile({
+    name: `.eiswald/${fileName}`
+  });
+  if (!target) {
+    throw new Error(`Failed to create File for ${filePath}`);
+  }
+  console.log(`${target.id}, ${target.url}, ${target.ttl}`);
+  const file = fileFromSync(filePath);
+  console.log(`Uploading ${fileName}`);
+  const response = await fetch(target.url, {
+    method: "PUT",
+    body: file
+  });
+  if (!response.ok) {
+    try {
+      await deleteFile({ id: target.id });
+    } catch (error) {
+      console.error(error);
+    }
+    throw new Error(`Failed to upload ${filePath}: ${response.statusText}`);
+  }
+  return target.id;
 }
 
 start()
